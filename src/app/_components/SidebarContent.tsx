b6f503ab92b63f20d6a3a5fb9a897943acf739df
Jake Mattila <jakemattila@yahoo.com>
2024-02-10 14:55:48 -0500

    Refactor sidebar content to use dynamic items

--- src/app/_components/SidebarContent.tsx
+++ src/app/_components/SidebarContent.tsx
@@ -1,23 +1,17 @@
 import React from "react";
 
-interface interfaceProps {
+interface sidebarContentProps {
     title: string,
-    one?: string,
-    two?: string,
-    three?: string,
-    four?: string,
-    five?: string,
+    items: string[]
 }
-const SideBarContent = ({title, one, two, three, four, five}: interfaceProps) => {
+const SideBarContent = ({ title, items }: sidebarContentProps) => {
     return (
         <div className="py-5 border-b-[1px] border-b-gray-300">
             <h3 className="text-lg font-titleFont font-medium mb-1 px-8">{title}</h3>
             <ul>
-                {one && <li className="flex items-center justify-between hover:bg-[#BAC4C8] px-8 py-3 cursor-pointer text-sm">{one}</li>}
-                {two && <li className="flex items-center justify-between hover:bg-[#BAC4C8] px-8 py-3 cursor-pointer text-sm">{two}</li>}
-                {three && <li className="flex items-center justify-between hover:bg-[#BAC4C8] px-8 py-3 cursor-pointer text-sm">{three}</li>}
-                {four && <li className="flex items-center justify-between hover:bg-[#BAC4C8] px-8 py-3 cursor-pointer text-sm">{four}</li>}
-                {five && <li className="flex items-center justify-between hover:bg-[#BAC4C8] px-8 py-3 cursor-pointer text-sm">{five}</li>}
+                {items.map((item, index) => (
+                    <li key={index} className="px-8 py-2 text-sm font-titleFont font-normal hover:bg-gray-100 cursor-pointer">{item}</li>
+                ))}
             </ul>
         </div>  
         
