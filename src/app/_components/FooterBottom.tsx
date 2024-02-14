1ff5936a18c0577268c6283103c15eaf69e73747
Jake Mattila <jakemattila@yahoo.com>
2024-02-14 13:28:29 -0500

    Add new files and update components in the codebase

---
+++ src/app/_components/FooterBottom.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import React from "react"
+
+const FooterBottom = () => {
+    return (
+        <div className="w-full bg-footerBottom py-8">
+            <div className="max-w-5xl mx-auto">
+                <p className="flex justify-center items-center text-white">Created by Group 9</p>
+            </div>
+        </div>
+    )
+};
+
+export default FooterBottom;
