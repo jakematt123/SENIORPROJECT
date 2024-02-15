9f82962bedb8d8c711b4355f9ff2900bba26ea17
Jake Mattila <jakemattila@yahoo.com>
2024-02-15 12:44:41 -0500

    Fix links in Bottomnavbar component

--- src/app/_constants/BottomBarItems.ts
+++ src/app/_constants/BottomBarItems.ts
@@ -1,7 +1,7 @@
 export const bottomBarItems = [
-    {text: "Today's Deals", url: "/"},
-    {text: "Customer Service", url: "/"},
-    {text: "Gift Cards", url: "/"},
-    {text: "Registry", url: "/"},
-    {text: "Sell", url: "/"},
+    {text: "Today's Deals", url: "/contact"},
+    {text: "Customer Service", url: "/contact"},
+    {text: "Gift Cards", url: "/contact"},
+    {text: "Registry", url: "/contact"},
+    {text: "Sell", url: "/contact"},
 ]
